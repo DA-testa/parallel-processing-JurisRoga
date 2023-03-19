1e66d2423db910206b70122a6f1aa2e0e88028e3
JurisRoga <124889862+JurisRoga@users.noreply.github.com>
2023-03-19 09:21:18 +0000

    tests_1

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ def parallel_processing(n, m, data):
     output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
-
+    beig_laiks_threads = [0] * n
+    nakam_thread = 0
+    for i in range(m):
+        thread = nakam_thread
+        sakum_laiks = beig_laiks_threads[thread]
+        beig_laiks = sakum_laiks + data[i]
+        beig_laiks_threads[thread] = beig_laiks
+        output.append((thread, sakum_laiks))
+        nakam_thread = (nakam_thread + 1) % n
     return output
 
 def main():
@@ -15,16 +23,18 @@ def main():
     # m - job count
     n = 0
     m = 0
-
+    n, m = map(int, input().split())
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
     data = []
+    data = list(map(int, input().split()))
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
     
     # TODO: print out the results, each pair in it's own line
-
+    for i in result:
+        print(i[0], i[1])
 
 
 if __name__ == "__main__":
